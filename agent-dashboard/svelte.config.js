3e3db57f44bc3abf57689c3e86dc1597fa5dbab7
Kevin (via Cortana) <kevin@users.noreply.github.com>
2026-03-05 09:26:13 +0100

    ensure assets path is absolute

--- agent-dashboard/svelte.config.js
+++ agent-dashboard/svelte.config.js
@@ -1,6 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 
 const basePath = process.env.BASE_PATH ?? '';
+const paths = {
+	base: basePath
+};
+
+if (basePath) {
+	paths.assets = basePath;
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,10 +17,7 @@ const config = {
 			assets: 'build',
 			fallback: '200.html'
 		}),
-		paths: {
-			base: basePath,
-			assets: basePath
-		},
+		paths,
 	}
 };
 
